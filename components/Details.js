7ca16314fb14ad29dd81c94c21b250b9b0a562bb
MVAodhan <hamilton.aodhan@gmail.com>
2022-04-04 17:33:52 +1200

    DRY code to add multiple sounds

--- components/Details.js
+++ components/Details.js
@@ -3,20 +3,23 @@ import { StyleSheet } from 'react-native';
 import React from 'react';
 
 import { Audio } from 'expo-av';
+import { useAssets } from 'expo-asset';
 
 import { Button, Text, Center, Box } from 'native-base';
 
 export default function Details({ navigation }) {
   const [sound, setSound] = useState();
 
-  const playSound = async (id) => {
-    console.log(id);
-    console.log('Loading Sound');
-    const { sound } = await Audio.Sound.createAsync(
-      require('../assets/nyi.mp3')
-    );
-    setSound(sound);
+  const [assets, error] = useAssets([
+    require('../assets/fbf.mp3'),
+    require('../assets/nyi.mp3'),
+  ]);
 
+  const playSound = async (assetNum) => {
+    console.log(assetNum);
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(assets[assetNum]);
+    setSound(sound);
     console.log('Playing Sound');
     await sound.playAsync();
   };
@@ -33,8 +36,10 @@ export default function Details({ navigation }) {
     <Box width="100%" height="100%">
       <Center width="100%" height="100%">
         <Text>Details</Text>
-
-        <Button onPress={() => playSound('idSound')}>Play Sound</Button>
+        <Button onPress={() => playSound(0)}>Play Sound</Button>
+        <Button mt="20px" onPress={() => playSound(1)}>
+          Play Sound
+        </Button>
       </Center>
     </Box>
   );
